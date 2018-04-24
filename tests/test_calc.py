954a344a23cccf68a6ea54c047d763113814e014
kaave <abe@framelunch.jp>
2018-04-24 21:40:50 +0900

    Add simple test

---
+++ tests/test_calc.py
@@ -0,0 +1,12 @@
+import unittest
+
+import calc
+
+
+class TestSample(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calc.sum(10, 20), 30)
+
+
+if __name__ == '__main__':
+    unittest.main()
